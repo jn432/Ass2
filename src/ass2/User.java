b7f50d60fbbdbffa5be91a242db12f24407c1180
FFMaster <FFMaster@HAL-9000>
2018-09-24 20:58:32 +1000

    add comments to User add unique id

--- src/ass2/User.java
+++ src/ass2/User.java
@@ -4,12 +4,25 @@ import java.io.Serializable;
 import java.util.Arrays;
 
 abstract class User implements Serializable {
+    
+    //id is used purely to keep track for administration purposes
+    private static int counter = 1;
+    protected int id;
+    
     protected String username;
     protected char[] password;
     
+    //constructor
     public User(String username, char[] password) {
+        id = counter++;
         this.username = username;
         this.password = password;
+    }
+    
+    
+    //get methods
+    public int getID() {
+        return id;
     }
     
     public String getUsername() {
@@ -19,11 +32,15 @@ abstract class User implements Serializable {
     public char[] getPassword() {
         return password;
     }
+    //end of get methods
     
+    //abstract methods that child classes must have
     abstract public int getUserType();
     
     abstract public String getDetails();
+    //end of abstract methods
     
+    //returns true if password matches the account's password, otherwise returns false
     public static boolean verifyUser(User u, char[] enteredPassword) {
         if (Arrays.equals(u.password, enteredPassword)) {
             return true;
