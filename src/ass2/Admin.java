4981084ca0528c423816937461eb95c425d826da
FFMaster <FFMaster@HAL-9000>
2018-09-24 22:08:43 +1000

    Added edit method

--- src/ass2/Admin.java
+++ src/ass2/Admin.java
@@ -17,6 +17,24 @@ class Admin extends User implements Serializable {
         return "\nAccount ID: " + id + "\nUsername: " + username + "\nPassword: " + password + "\n";
     }
     
+    public static boolean editUser(User u, String username, char[] password, String degree) {
+        User checkDuplicate = Ass2.LIB.findUser(username);
+        //if username is already used by an account and is not the same user
+        if (checkDuplicate != null && u != checkDuplicate) {
+            System.err.println("User not edited");
+            return false;
+        }
+        u.username = username;
+        u.password = password;
+        //if user is a student
+        if (u.getUserType() == 1) {
+            Student s = (Student) u;
+            s.setDegree(degree);
+        }
+        System.err.println("User edited");
+        return true;
+    }
+    
     public static boolean createStudent(String username, char[] password, String degree) {
         User u = Ass2.LIB.findUser(username);
         if (u == null) {
