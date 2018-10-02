8b55180de57ef9b2015d7930abbe44293da84fdb
FFMaster <FFMaster@HAL-9000>
2018-10-02 13:08:35 +1000

    changed the global variables to final

--- src/ass2/Library.java
+++ src/ass2/Library.java
@@ -6,44 +6,44 @@ import java.nio.file.*;
 
 class Library implements Serializable {
     
-    private String libraryFile;
+    private final String LIBRARYFILE;
     
-    private HashMap<Integer, Book> bookMap;
-    private HashMap<String, User> userMap;
+    private final HashMap<Integer, Book> BOOKMAP;
+    private final HashMap<String, User> USERMAP;
     
     //get methods
     public HashMap<Integer, Book> getBooks() {
-        return bookMap;
+        return BOOKMAP;
     }
     
     public HashMap<String, User> getUsers() {
-        return userMap;
+        return USERMAP;
     }
     //end of get methods
     
     //find a user in the library
     public User findUser(String username) {
-        return userMap.get(username);
+        return USERMAP.get(username);
     }
     
     //find a book in the library
     public Book findBook(int ISBN) {
-        return bookMap.get(ISBN);
+        return BOOKMAP.get(ISBN);
     }
     
     //load or create library on initializatoin
     public Library(String libraryFile) {
         //set the file path
-        this.libraryFile = libraryFile;
+        LIBRARYFILE = libraryFile;
         //create the hashmaps for users and books
-        bookMap = new HashMap<>();
-        userMap = new HashMap<>();
+        BOOKMAP = new HashMap<>();
+        USERMAP = new HashMap<>();
     }
     
     //save the library after an update
     public void saveLibrary() {
         try {
-            ObjectOutputStream output = new ObjectOutputStream(Files.newOutputStream(Paths.get(libraryFile)));
+            ObjectOutputStream output = new ObjectOutputStream(Files.newOutputStream(Paths.get(LIBRARYFILE)));
             output.writeObject(this);
             output.close();
             System.err.println("Library saved");
